7c2697d6590039f2d8a8224f67768e4a6f8645c9
miumo <miumo2020@gmail.com>
2021-06-07 19:57:45 +0900

    押弦表示追加、状態管理class作成

--- js/guitar-code-palette.js
+++ js/guitar-code-palette.js
@@ -1,37 +1,139 @@
+const MAX_STRING = 6;
+const MAX_FLET = 5;
+
+
+class FingerBoard {
+    constructor() {
+        this.tuning = [64, 59, 55, 50, 45, 40];
+        this.press_point = [0, 0, 0, 0, 0, 0];
+    }
+
+    SCALE_DICT = {
+        "5": [0,7],
+        "maj": [0,4,7],
+        "m": [0,3,7],
+        "(-5)": [0,4,6],
+        "m-5": [0,3,6],
+        "aug": [0,4,8],
+        "sus4": [0,5,7],
+        "sus2": [0,2,7],
+    }
+
+    pressString(string, flet) {
+        if (this.press_point[string-1] == flet) {
+            this.press_point[string-1] = 0;
+        } else {
+            this.press_point[string-1] = flet;
+        }
+        this.update();
+        this.predictCode();
+    }
+
+    update() {
+        for(let i=1; i<=MAX_FLET; i++) {
+            for(let j=1; j<=MAX_STRING; j++) {
+                let p = document.getElementById('string-press-' + j + '-flet-' + i);
+                if (this.press_point[j-1] == i) {
+                    p.style.visibility = 'visible'
+                } else {
+                    p.style.visibility = 'hidden'
+                }   
+            }
+        }
+    }
+
+    predictCode() {
+        let root = this.getRoot();
+        let scale = this.getNowScale();
+        console.log(scale);
+        const code = Object.keys(this.SCALE_DICT).filter((key) => {
+            return this.SCALE_DICT[key].toString() === scale.toString();
+        });
+        console.log(code);
+    }
+
+    getRoot() {
+        let scale = [];
+        for(let j=0; j<=MAX_STRING-1; j++) {
+            scale.push(this.tuning[j] + this.press_point[j]);
+        }
+        scale.sort(function(a,b){
+            return (a < b ? -1 : 1);
+        });
+        let root = scale[0];
+        return root;
+    }
+
+    getNowScale() {
+        let scale = [];
+        for(let j=0; j<=MAX_STRING-1; j++) {
+            scale.push(this.tuning[j] + this.press_point[j]);
+        }
+        scale.sort(function(a,b){
+            return (a < b ? -1 : 1);
+        });
+        let root = scale[0];
+        for(let i=0; i<scale.length; i++) {
+            scale[i] = scale[i] - root;
+        }
+        for(let i=0; i<scale.length; i++) {
+            const octave_down = scale[i] - 12;
+            if ((12 > octave_down) && (octave_down >= 0)) {
+                scale[i] = scale[i] - 12;
+            }
+            else if ((24 > octave_down) && (octave_down >= 12)) {
+                scale[i] = scale[i] - 24;
+            }
+        }
+        scale = Array.from(new Set(scale));
+        scale.sort(function(a,b){
+            return (a < b ? -1 : 1);
+        });
+        return scale;
+    }
+
+    reset() {
+        this.press_point = [0, 0, 0, 0, 0, 0];
+        this.update();
+    }
+}
+
+let fg = new FingerBoard();
+
+
+
 const app = document.getElementById('app');
 
 let finger_board = document.createElement('div');
 finger_board.className = 'finger-board';
 app.appendChild(finger_board);
 
-// // 0flet
-// let d_ = document.createElement('div');
-// finger_board.appendChild(d_);
-// // 6stringループ
-// for(let j=1; j<=6; j++) {
-//     let string_area_ = document.createElement('div');
-//     string_area_.className = 'string-area';
-
-//     d_.appendChild(string_area_);
-// }
-
 let nut = document.createElement('div');
 nut.className = 'nut';
 finger_board.appendChild(nut);
 
 // 15fletループ
-for(let i=1; i<=15; i++) {
+for(let i=1; i<=MAX_FLET; i++) {
     let d = document.createElement('div');
     finger_board.appendChild(d);
     // 6stringループ
-    for(let j=1; j<=6; j++) {
+    for(let j=1; j<=MAX_STRING; j++) {
         let string = document.createElement('div');
         string.className = 'string';
         string.id = 'string-' + j + '-flet-' + i;
 
+        let string_press = document.createElement('div');
+        string_press.className = 'string-press';
+        string_press.id = 'string-press-' + j + '-flet-' + i;
+
         let string_area = document.createElement('div');
         string_area.className = 'string-area';
         string_area.appendChild(string);
+        string_area.appendChild(string_press);
+
+        string_area.addEventListener('click', function() {
+            fg.pressString(j, i);
+        });
 
         d.appendChild(string_area);
     }
