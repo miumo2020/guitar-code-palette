904cf61182e4e3d9d7cc3ee1f403eafd3f3e2f5b
miumo <miumo2020@gmail.com>
2021-06-19 16:26:09 +0900

    setPressPoint修正

--- js/finger-board.js
+++ js/finger-board.js
@@ -21,6 +21,7 @@ class String extends React.Component {
     this.setState({
       position: new_pos,
     });
+    this.setPressPoint(this.state.number, new_pos)
   }
 
   render() {
@@ -31,31 +32,18 @@ class String extends React.Component {
         flet_0 = "×"
     }
     return(
-      // e("div", { class: "string2", id: this.state.name }, [
-      //   e("div", { class: "flet2", onClick: ()=>this.press(0)}, [flet_0]),
-      //   e("div", { class: "flet2", onClick: ()=>this.press(1)}, [this.state.position == 1 ? "●" : ""]),
-      //   e("div", { class: "flet2", onClick: ()=>this.press(2)}, [this.state.position == 2 ? "●" : ""]),
-      //   e("div", { class: "flet2", onClick: ()=>this.press(3)}, [this.state.position == 3 ? "●" : ""]),
-      //   e("div", { class: "flet2", onClick: ()=>this.press(4)}, [this.state.position == 4 ? "●" : ""]),      
-      //   e("div", { class: "flet2", onClick: ()=>this.press(5)}, [this.state.position == 5 ? "●" : ""]),      
-      //   e("div", { class: "flet2", onClick: ()=>this.press(6)}, [this.state.position == 6 ? "●" : ""]),      
-      //   e("div", { class: "flet2", onClick: ()=>this.press(7)}, [this.state.position == 7 ? "●" : ""]),      
-      //   e("div", { class: "flet2", onClick: ()=>this.press(8)}, [this.state.position == 8 ? "●" : ""]),      
-      //   e("div", { class: "flet2", onClick: ()=>this.press(9)}, [this.state.position == 9 ? "●" : ""]),      
-      //   e("div", { class: "flet2", onClick: ()=>this.press(10)}, [this.state.position == 10 ? "●" : ""]),      
-      // ])
       e("div", { class: "string2", id: this.state.name }, [
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 0)}, [flet_0]),
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 1)}, [this.state.position == 1 ? "●" : ""]),
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 2)}, [this.state.position == 2 ? "●" : ""]),
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 3)}, [this.state.position == 3 ? "●" : ""]),
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 4)}, [this.state.position == 4 ? "●" : ""]),      
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 5)}, [this.state.position == 5 ? "●" : ""]),      
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 6)}, [this.state.position == 6 ? "●" : ""]),      
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 7)}, [this.state.position == 7 ? "●" : ""]),      
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 8)}, [this.state.position == 8 ? "●" : ""]),      
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 9)}, [this.state.position == 9 ? "●" : ""]),      
-        e("div", { class: "flet2", onClick: ()=>this.setPressPoint(this.state.number, 10)}, [this.state.position == 10 ? "●" : ""]),      
+        e("div", { class: "flet2", onClick: ()=>this.press(0)}, [flet_0]),
+        e("div", { class: "flet2", onClick: ()=>this.press(1)}, [this.state.position == 1 ? "●" : ""]),
+        e("div", { class: "flet2", onClick: ()=>this.press(2)}, [this.state.position == 2 ? "●" : ""]),
+        e("div", { class: "flet2", onClick: ()=>this.press(3)}, [this.state.position == 3 ? "●" : ""]),
+        e("div", { class: "flet2", onClick: ()=>this.press(4)}, [this.state.position == 4 ? "●" : ""]),      
+        e("div", { class: "flet2", onClick: ()=>this.press(5)}, [this.state.position == 5 ? "●" : ""]),      
+        e("div", { class: "flet2", onClick: ()=>this.press(6)}, [this.state.position == 6 ? "●" : ""]),      
+        e("div", { class: "flet2", onClick: ()=>this.press(7)}, [this.state.position == 7 ? "●" : ""]),      
+        e("div", { class: "flet2", onClick: ()=>this.press(8)}, [this.state.position == 8 ? "●" : ""]),      
+        e("div", { class: "flet2", onClick: ()=>this.press(9)}, [this.state.position == 9 ? "●" : ""]),      
+        e("div", { class: "flet2", onClick: ()=>this.press(10)}, [this.state.position == 10 ? "●" : ""]),      
       ])
     )
   }
@@ -71,22 +59,15 @@ export class FingerBoard2 extends React.Component {
     this.setPressPoint = this.setPressPoint.bind(this);
   }
 
-
   setPressPoint = (string, point) => {
     console.log(string, point);
+    let new_press_point = this.state.press_point;
+    new_press_point[string-1] = point
+    this.setState({press_point: this.state.press_point})
   }
 
-  // setPressPoint = (string, point) => {
-  //   console.log(string, point);
-  //   let new_press_point = this.state.press_point;
-  //   new_press_point[string-1] = point
-  //   // this.setState({press_point: this.state.press_point})
-  //   this.setState((new_press_point) => {
-  //     return {press_point: new_press_point}
-  //   });
-  // }
-
   render() {
+    console.log(this.state.press_point);
     return e("div", {}, [
       e("div", {}, [this.state.press_point]),
       e(String, { number: 1, setPressPoint: this.setPressPoint }, []),
