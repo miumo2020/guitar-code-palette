c2d3ff4174ff5c4651df31e867c6d988398b4a76
miumo <miumo2020@gmail.com>
2021-07-06 06:49:31 +0900

    リファクタリング：不要コード削除、stateにする必要がないものをpropsに変更

--- js/code-menu.js
+++ js/code-menu.js
@@ -5,25 +5,20 @@ const e = React.createElement;
 class Code extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {
-        name: props.name,
-        positions: props.positions,
-        children: props.children,
-      };
-      this.call = props.call;
+      this.call = props.call.bind(this);
     }
     
     render() {
-      if (this.state.children.length != 0) {
-        return e("li", {key: this.state.name + " li"}, [
-          e("a", { key: this.state.name + " li a", href: "#", onClick: () => this.call(this.state.name, 
-            this.state.positions[0])}, this.state.name),
-          e("ul", {key: this.state.name + " li a ul"}, this.state.children),
+      if (this.props.children.length != 0) {
+        return e("li", {key: this.props.name + " li"}, [
+          e("a", { key: this.props.name + " li a", href: "#", onClick: () => this.call(this.props.name, 
+            this.props.positions[0])}, this.props.name),
+          e("ul", {key: this.props.name + " li a ul"}, this.props.children),
         ]);
       } else {
-        return e("li", {key: this.state.name + " li"}, [
-          e("a", { key: this.state.name + " li a", href: "#" , onClick: () => this.call(this.state.name, 
-            this.state.positions[0])}, this.state.name)
+        return e("li", {key: this.props.name + " li"}, [
+          e("a", { key: this.props.name + " li a", href: "#" , onClick: () => this.call(this.props.name, 
+            this.props.positions[0])}, this.props.name)
         ]);
       }
     }
@@ -34,23 +29,6 @@ export class CodeMenu extends React.Component {
     super(props);
     this.state = {};
   }
-
-  CODE_MENU = {
-    C: {
-      position: [[0, 1, 0, 2, 3, -1]],
-      child: {
-        C: {
-          position: [[0, 1, 0, 2, 3, -1]],
-          child: {
-            C6: { position: [[0, 1, 2, 2, 3, -1]] },
-            C7: { position: [[0, 1, 3, 2, 3, -1]] },
-            Cmaj7: { position: [[0, 0, 0, 2, 3, -1]] },
-            Cadd9: { position: [[0, 3, 0, 2, 3, -1]] },
-          },
-        },
-      },
-    },
-  };
 
   // TODO: 一時的にCODEクラスに本関数を渡しているが、押弦を直接操作する関数に変更する
   call(name, position) {
@@ -67,14 +45,14 @@ export class CodeMenu extends React.Component {
               e(Code, {key: "C6(9)", name: "C6(9)", positions: [], call: this.call, children: []}),              
             ],}),
             e(Code, {key: "C7", name: "C7", positions: [], call: this.call, children: [
-              e(Code, {key: "C7(♭9)", name: "C7(♭9)", positions: [], call: this.call, children: []}),
+              e(Code, {key: "C7(b9)", name: "C7(b9)", positions: [], call: this.call, children: []}),
               e(Code, {key: "C7(9)", name: "C7(9)", positions: [], call: this.call, children: []}),
               e(Code, {key: "C7(#9)", name: "C7(#9)", positions: [], call: this.call, children: []}),
               e(Code, {key: "C7(11)", name: "C7(11)", positions: [], call: this.call, children: []}),
               e(Code, {key: "C7(#11)", name: "C7(#11)", positions: [], call: this.call, children: []}),
-              e(Code, {key: "C7(♭13)", name: "C7(♭13)", positions: [], call: this.call, children: []}),
+              e(Code, {key: "C7(b13)", name: "C7(b13)", positions: [], call: this.call, children: []}),
               e(Code, {key: "C7(13)", name: "C7(13)", positions: [], call: this.call, children: []}),
-              e(Code, {key: "C7(♭5)", name: "C7(♭5)", positions: [], call: this.call, children: []}),
+              e(Code, {key: "C7(b5)", name: "C7(b5)", positions: [], call: this.call, children: []}),
               e(Code, {key: "C7(5#)", name: "C7(5#)", positions: [], call: this.call, children: []}),
             ],}),
             e(Code, {key: "Cmaj7", name: "Cmaj7", positions: [], call: this.call, children: [
