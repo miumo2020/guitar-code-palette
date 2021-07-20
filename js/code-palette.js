2b763b96860df106c82759a21c3a9d2fa1b66bcb
miumo <miumo2020@gmail.com>
2021-07-20 12:05:10 +0900

    パレット登録処理作成

--- js/code-palette.js
+++ js/code-palette.js
@@ -2,11 +2,30 @@
 
 const e = React.createElement;
 
+const MAX_PALETTE = 16;
+
+class Code extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (this.props.position == null) return "";
+
+    let code_name = this.props.codeName
+    if (code_name == null) {
+      code_name = "Unknown Code"
+    }
+    return [code_name, e("br", { key: "br" }), this.props.position];
+  }
+}
+
 export class CodePalette extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        selected_palette: 1,
+      selected_palette: 0,
+      code_palette: Array(MAX_PALETTE).fill({ code_name: null, position: null }),
     };
     this.selectPalette = this.selectPalette.bind(this);
   }
@@ -17,26 +36,40 @@ export class CodePalette extends React.Component {
 
   renderPalettes() {
     let result = [];
-    for (let i=1; i<=16; i++) {
+    for (let i = 0; i < MAX_PALETTE; i++) {
       let class_name = "palette";
       if (i == this.state.selected_palette) {
         class_name = "selected-palette";
       }
       result.push(
-        e("div", {key: "palette-"+String(i), className: class_name, onClick: () => this.selectPalette(i)}, [])
+        e("div", { key: "palette-" + String(i), className: class_name, onClick: () => this.selectPalette(i) }, [
+          e(Code, { key: "palette-code-" + String(i), codeName: this.state.code_palette[i]["code_name"], position: this.state.code_palette[i]["position"] })
+        ])
       );
     }
     return result;
   }
 
   registerPalette(code_name, position) {
-    console.log(code_name, position);
+    // 全弦押弦なしの場合、処理終了
+    if (position.every((element) => { return element == -1; })) return;
+
+    // 1弦でも押弦がある場合、パレット登録
+    // パレット0から順番に参照し、空いているパレットに登録する
+    let new_code_palette = [...this.state.code_palette];
+    for (let i = 0; i < MAX_PALETTE; i++) {
+      if ((this.state.code_palette[i]["code_name"] == null) && (this.state.code_palette[i]["position"] == null)) {
+        new_code_palette[i] = { code_name: code_name, position: position };
+        break;
+      }
+    }
+    this.setState({ code_palette: new_code_palette });
   }
 
   render() {
     return [
-      e("div", {key: "code-palette"}, ["Code Palette"]),
-      e("div", {key: "palette-wrapper", className: "wrapper-flex"}, this.renderPalettes()),          
+      e("div", { key: "code-palette" }, ["Code Palette"]),
+      e("div", { key: "palette-wrapper", className: "wrapper-flex" }, this.renderPalettes()),
     ];
   }
 }
