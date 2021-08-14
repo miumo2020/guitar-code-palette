bded38b25cb324deae5a1140432c28e58d7fbc18
miumo <miumo2020@gmail.com>
2021-08-15 00:00:23 +0900

    メニューボックス操作時、範囲外クリックでメニューボックスを閉じる処理

--- js/code-palette-menu.js
+++ js/code-palette-menu.js
@@ -6,18 +6,22 @@ export class CodePaletteMenu extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.ref = React.createRef();
+    this.ref_menu = React.createRef();
+    this.ref_out = React.createRef();
     this.onMenuItemClick = this.onMenuItemClick.bind(this);
+    this.onOutOfRangeClick = this.onOutOfRangeClick.bind(this);
   }
 
   show(pos_x, pos_y) {
-    this.ref.current.style.top = pos_y + "px";
-    this.ref.current.style.left = pos_x + "px";
-    this.ref.current.style.visibility = "visible";
+    this.ref_menu.current.style.top = pos_y + "px";
+    this.ref_menu.current.style.left = pos_x + "px";
+    this.ref_menu.current.style.visibility = "visible";
+    this.ref_out.current.style.visibility = "visible";
   }
 
   close() {
-    this.ref.current.style.visibility = "hidden";
+    this.ref_menu.current.style.visibility = "hidden";
+    this.ref_out.current.style.visibility = "hidden";
   }
 
   onMenuItemClick(event) {
@@ -25,13 +29,21 @@ export class CodePaletteMenu extends React.Component {
     this.close();
   }
 
+  onOutOfRangeClick(event) {
+    this.close();
+  }
+
   render() {
     return [
-      e("div", { key: "menu-box", className: "palette-menu-box", ref: this.ref }, [
+      e("div", { key: "menu-box", className: "palette-menu-box", ref: this.ref_menu }, [
         e("div", { key: "menu-item-1", className: "palette-menu-item", onClick: this.onMenuItemClick }, ["item-1"]),
         e("div", { key: "menu-item-2", className: "palette-menu-item", onClick: this.onMenuItemClick }, ["item-2"]),
         e("div", { key: "menu-item-3", className: "palette-menu-item", onClick: this.onMenuItemClick }, ["item-3"]),
-      ])
+      ]),
+      e("div", {
+        key: "out-of-range-menu-box", className: "out-of-range-menu-box",
+        onClick: this.onOutOfRangeClick, ref: this.ref_out
+      })
     ];
   }
 }
